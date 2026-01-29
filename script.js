0f9062df54f53f1b09ed5abcb61c84d95171526d
alicoder123411 <sameersingla240@gmail.com>
2026-01-29 16:54:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const time = document.getElementById("delay");
+const message = document.getElementById("text");
+const output = document.getElementById("output");
+
+function promise1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(message.value);
+    }, Number(time.value));
+  });
+}
+
+async function handlepromise(e) {
+  e.preventDefault();
+
+  let data;
+  try {
+    data = await promise1();
+  } catch (e) {
+    console.log("error", e);
+  }
+
+  if (data) {
+    output.innerText = data;
+  }
+}
+
+document.getElementById("btn").addEventListener("click", handlepromise);
